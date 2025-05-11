01ae3612ee1412d513497d2864695309de4e2ad2
Michael <machapurin@icloud.com>
2025-05-12 00:04:09 +0300

    add skeleton and metadata to sku-page

--- src/app/catalog/[sku]/page.tsx
+++ src/app/catalog/[sku]/page.tsx
@@ -5,6 +5,7 @@ import {
 	SEARCH_PARAMS,
 	TAB_VALUES
 } from '@/shared/settings'
+
 import {
 	Title,
 	Paragraph,
@@ -12,6 +13,7 @@ import {
 	Button,
 	Accordion
 } from '@/shared/ui'
+
 import { calculateAverage, declareOfNumber } from '@/shared/utils'
 import { cn } from '@/shared/lib'
 
@@ -20,7 +22,9 @@ import {
 	CartCounter,
 	FloatTabIndicator,
 	LikeButton,
-	Rating
+	Rating,
+	SharedButton,
+	SharedButtonList
 } from '@/features'
 
 import {
@@ -32,14 +36,31 @@ import {
 	FormReview
 } from '@/widgets'
 
-import { Suspense } from 'react'
+import { Metadata } from 'next'
 
 import styles from './page.module.css'
-import { SharedButton, SharedButtonList } from '@/features/shared-button'
 
 type Params = Promise<{ sku: string }>
 
+export async function generateMetadata({
+	params
+}: {
+	params: Params
+}): Promise<Metadata> {
+	const { sku } = await params
+	const data = await fetch(API_URLS.PRODUCT_SKU + sku)
+	const product: ProductType = await data.json()
+
+	return {
+		title: `Купить ${product.name}`,
+		description: product.description
+	}
+}
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
 export default async function ProductPage({ params }: { params: Params }) {
+	await delay(10000)
 	const { sku } = await params
 	const data = await fetch(API_URLS.PRODUCT_SKU + sku)
 	const product: ProductType = await data.json()
@@ -85,131 +106,129 @@ export default async function ProductPage({ params }: { params: Params }) {
 	)
 
 	return (
-		<Suspense>
-			<main className={styles.detail}>
-				<section className={styles.detail__product}>
-					<VisuallyHiddenTitle>
-						{`Купить ${categoryName} ${product.name}`}
-					</VisuallyHiddenTitle>
-					<div className={styles.detail__galery}>
-						<Galery images={product.images.slice(0, 4)} name={product.name} />
-					</div>
-					<div className={styles.product}>
-						<Title As='h2' className={styles.product__title} size='lg'>
-							{product.name}
-						</Title>
-						<div className={cn(styles.product__price, styles.product__row)}>
-							{isDiscount && (
-								<Paragraph
-									className={styles['product__price--old']}
-									color='error'
-								>
-									$ {product.price.toFixed(2)}
-								</Paragraph>
-							)}
-							<Paragraph className={styles.product__price} color='primary'>
-								$ {productPrice}
-							</Paragraph>
-							<SharedButton className={styles.product__shared} />
-						</div>
-						<div className={styles.desktop}>
-							<AnchorLink
-								className={cn(styles['product__review-link'])}
-								href={'#' + PAGES_ID.SKU_DETAIL}
-								id={PAGES_ID.SKU_DETAIL}
-								params={SEARCH_PARAMS.ACTIVE_TAB}
-								value={TAB_VALUES.REVIEWS}
+		<main className={styles.detail}>
+			<section className={styles.detail__product}>
+				<VisuallyHiddenTitle>
+					{`Купить ${categoryName} ${product.name}`}
+				</VisuallyHiddenTitle>
+				<div className={styles.detail__galery}>
+					<Galery images={product.images.slice(0, 4)} name={product.name} />
+				</div>
+				<div className={styles.product}>
+					<Title As='h2' className={styles.product__title} size='lg'>
+						{product.name}
+					</Title>
+					<div className={cn(styles.product__price, styles.product__row)}>
+						{isDiscount && (
+							<Paragraph
+								className={styles['product__price--old']}
+								color='error'
 							>
-								<Rating value={averageRating} />
-								<span>
-									{declareOfNumber(
-										reviewsLength,
-										['отзыв', 'отзыва', 'отзывов'],
-										true
-									)}
-								</span>
-							</AnchorLink>
-							<AnchorLink
-								className={cn(
-									styles['product__description-link'],
-									styles.desktop
-								)}
-								href={'#' + PAGES_ID.SKU_DETAIL}
-								id={PAGES_ID.SKU_DETAIL}
-								params={SEARCH_PARAMS.ACTIVE_TAB}
-								value={TAB_VALUES.DESCRIPTION}
-							>
-								<Paragraph
-									className={styles['product--truncate-text']}
-									color='secondary'
-								>
-									{product.description}
-								</Paragraph>
-							</AnchorLink>
-						</div>
-						<div className={styles.product__cart}>
-							<div className={styles.product__counter}>
-								<CartCounter sku={product.sku} />
-							</div>
-							<Button className={styles.product__button} variant='outline'>
-								Добавить в корзину
-							</Button>
-						</div>
-						<div className={styles.product__actions}>
-							<LikeButton sku={+sku} />
-							<SharedButtonList className={styles.product__socials} />
-						</div>
-						<div
-							className={cn(styles.product__property, styles.desktop)}
-							role='table'
-						>
-							<Paragraph className={styles.product__row} role='rowgroup'>
-								<span role='cell'>SKU: </span>
-								<span role='cell' className={styles['product__property-value']}>
-									{product.sku}
-								</span>
+								$ {product.price.toFixed(2)}
 							</Paragraph>
-							<Paragraph className={styles.product__row} role='rowgroup'>
-								<span role='cell'>Категория: </span>
-								<span role='cell' className={styles['product__property-value']}>
-									{categoryName}
-								</span>
-							</Paragraph>
-						</div>
+						)}
+						<Paragraph className={styles.product__price} color='primary'>
+							$ {productPrice}
+						</Paragraph>
+						<SharedButton className={styles.product__shared} />
 					</div>
-				</section>
-				<section id={PAGES_ID.SKU_DETAIL} className={styles.detail__reviews}>
 					<div className={styles.desktop}>
-						<Tabs.TabControls>
-							<Tabs.TabButton tabId={TAB_VALUES.DESCRIPTION}>
-								Описание
-							</Tabs.TabButton>
-							<Tabs.TabButton tabId={TAB_VALUES.REVIEWS}>
-								Отзывы {`(${reviewsLength})`}
-							</Tabs.TabButton>
-						</Tabs.TabControls>
-						<FloatTabIndicator />
-						<Tabs.TabContent>
-							<Tabs.TabPanel tabId={TAB_VALUES.DESCRIPTION}>
-								<Description />
-							</Tabs.TabPanel>
-							<Tabs.TabPanel tabId={TAB_VALUES.REVIEWS}>
-								<div className={styles.reviews}>
-									<ReviewContent />
-								</div>
-							</Tabs.TabPanel>
-						</Tabs.TabContent>
+						<AnchorLink
+							className={cn(styles['product__review-link'])}
+							href={'#' + PAGES_ID.SKU_DETAIL}
+							id={PAGES_ID.SKU_DETAIL}
+							params={SEARCH_PARAMS.ACTIVE_TAB}
+							value={TAB_VALUES.REVIEWS}
+						>
+							<Rating value={averageRating} />
+							<span>
+								{declareOfNumber(
+									reviewsLength,
+									['отзыв', 'отзыва', 'отзывов'],
+									true
+								)}
+							</span>
+						</AnchorLink>
+						<AnchorLink
+							className={cn(
+								styles['product__description-link'],
+								styles.desktop
+							)}
+							href={'#' + PAGES_ID.SKU_DETAIL}
+							id={PAGES_ID.SKU_DETAIL}
+							params={SEARCH_PARAMS.ACTIVE_TAB}
+							value={TAB_VALUES.DESCRIPTION}
+						>
+							<Paragraph
+								className={styles['product--truncate-text']}
+								color='secondary'
+							>
+								{product.description}
+							</Paragraph>
+						</AnchorLink>
 					</div>
-					<div className={styles.mobile}>
-						<Accordion title='Описание'>
+					<div className={styles.product__cart}>
+						<div className={styles.product__counter}>
+							<CartCounter sku={product.sku} />
+						</div>
+						<Button className={styles.product__button} variant='outline'>
+							Добавить в корзину
+						</Button>
+					</div>
+					<div className={styles.product__actions}>
+						<LikeButton sku={+sku} />
+						<SharedButtonList className={styles.product__socials} />
+					</div>
+					<div
+						className={cn(styles.product__property, styles.desktop)}
+						role='table'
+					>
+						<Paragraph className={styles.product__rowProperty} role='rowgroup'>
+							<span role='cell'>SKU: </span>
+							<span role='cell' className={styles['product__property-value']}>
+								{product.sku}
+							</span>
+						</Paragraph>
+						<Paragraph className={styles.product__rowProperty} role='rowgroup'>
+							<span role='cell'>Категория: </span>
+							<span role='cell' className={styles['product__property-value']}>
+								{categoryName}
+							</span>
+						</Paragraph>
+					</div>
+				</div>
+			</section>
+			<section id={PAGES_ID.SKU_DETAIL} className={styles.detail__reviews}>
+				<div className={styles.desktop}>
+					<Tabs.TabControls>
+						<Tabs.TabButton tabId={TAB_VALUES.DESCRIPTION}>
+							Описание
+						</Tabs.TabButton>
+						<Tabs.TabButton tabId={TAB_VALUES.REVIEWS}>
+							Отзывы {`(${reviewsLength})`}
+						</Tabs.TabButton>
+					</Tabs.TabControls>
+					<FloatTabIndicator />
+					<Tabs.TabContent>
+						<Tabs.TabPanel tabId={TAB_VALUES.DESCRIPTION}>
 							<Description />
-						</Accordion>
-						<Accordion title={`Отзывы (${reviewsLength})`}>
-							<ReviewContent />
-						</Accordion>
-					</div>
-				</section>
-			</main>
-		</Suspense>
+						</Tabs.TabPanel>
+						<Tabs.TabPanel tabId={TAB_VALUES.REVIEWS}>
+							<div className={styles.reviews}>
+								<ReviewContent />
+							</div>
+						</Tabs.TabPanel>
+					</Tabs.TabContent>
+				</div>
+				<div className={styles.mobile}>
+					<Accordion title='Описание'>
+						<Description />
+					</Accordion>
+					<Accordion title={`Отзывы (${reviewsLength})`}>
+						<ReviewContent />
+					</Accordion>
+				</div>
+			</section>
+		</main>
 	)
 }

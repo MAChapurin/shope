fbf9d85c76b841e190874dff7510257ddcded89f
Michael <machapurin@icloud.com>
2025-05-13 22:50:18 +0300

    add next for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,16 +6,24 @@ export default function AboutPage() {
 	return (
 		<main className={styles.main}>
 			<h1 className={styles.title}>О нас</h1>
-			<p className={styles.subtitle}>Мы делаем шикарные украшения для вас</p>
-			<p className={styles.description}>
-				Duis rutrum dictum libero quis rutrum. Etiam sed neque aliquam,
-				sollicitudin ante&nbsp;a, gravida arcu. Nam fringilla molestie velit,
-				eget pellentesque risus scelerisque&nbsp;a. Nam ac&nbsp;urna maximus,
-				tempor magna&nbsp;et, placerat urna. Curabitur eu&nbsp;magna enim. Proin
-				placerat tortor lacus, ac&nbsp;sodales lectus placerat quis.
+			<p className={styles.subtitle}>
+				Добро пожаловать в&nbsp;Shope&nbsp;&mdash; ваш надежный магазин
+				ювелирных украшений, где каждый найдет что-то особенное!
 			</p>
 			<article className={styles.article}>
-				<h2 className={styles.article__title}>Тренды в украшениях</h2>
+				<h2 className={styles.article__title}>
+					<h2 className={styles.article__title}>Наша история</h2>
+				</h2>
+				<p className={styles.description}>
+					Магазин Shope был основан с&nbsp;любовью к&nbsp;красоте
+					и&nbsp;стремлением создавать уникальные украшения, которые
+					подчеркивают индивидуальность каждого клиента. Мы&nbsp;работаем
+					на&nbsp;рынке ювелирных изделий уже более 10&nbsp;лет, постоянно
+					расширяя ассортимент и&nbsp;совершенствуя качество.
+				</p>
+			</article>
+			<article className={styles.article}>
+				<h2 className={styles.article__title}>Наши ценности</h2>
 				<Image
 					className={styles.article__img}
 					width={670}
@@ -24,18 +32,21 @@ export default function AboutPage() {
 					alt='Изображение руки с бусами'
 					title='Тренды в украшениях'
 				/>
-				<p className={styles.article__paragraph}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-					placerat, augue a&nbsp;volutpat hendrerit, sapien tortor faucibus
-					augue, a&nbsp;maximus elit ex&nbsp;vitae libero. Sed quis mauris eget
-					arcu facilisis consequat sed eu&nbsp;felis.
-				</p>
 				<ul className={styles.article__list}>
 					<li className={styles.article__item}>
-						● Consectetur adipiscing elit. Aliquam placerat{' '}
+						● <b>Качество и&nbsp;оригинальность:</b> Вся наша продукция
+						создается из&nbsp;высококачественных материалов
+						и&nbsp;с&nbsp;вниманием к&nbsp;деталям.
 					</li>
 					<li className={styles.article__item}>
-						● Lorem ipsum dolor sit amet consectetur
+						● <b>Индивидуальный подход:</b> Мы&nbsp;ценим каждого клиента
+						и&nbsp;готовы помочь подобрать украшение, которое идеально подойдет
+						именно вам.
+					</li>
+					<li className={styles.article__item}>
+						● <b>Доверие и&nbsp;надежность:</b> Для нас важно, чтобы каждый
+						покупатель остался доволен своей покупкой и&nbsp;возвращался
+						к&nbsp;нам снова.
 					</li>
 				</ul>
 			</article>
@@ -50,11 +61,11 @@ export default function AboutPage() {
 					title='Сделано с любовью'
 				/>
 				<p className={styles.article__paragraph}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-					placerat, augue a&nbsp;volutpat hendrerit, sapien tortor faucibus
-					augue, a&nbsp;maximus elit ex&nbsp;vitae libero. Sed quis mauris eget
-					arcu facilisis consequat sed eu&nbsp;felis. Nunc sed porta augue.
-					Morbi porta tempor odio, in&nbsp;molestie diam bibendu.
+					В&nbsp;Shope мы&nbsp;объединяем традиции мастерства и&nbsp;современные
+					тенденции моды. Наши украшения&nbsp;&mdash; это не&nbsp;просто
+					аксессуары, а&nbsp;выражение вашей индивидуальности. Мы&nbsp;заботимся
+					о&nbsp;каждом клиенте и&nbsp;делаем все возможное, чтобы ваше
+					впечатление от&nbsp;покупки было максимально приятным.
 				</p>
 			</article>
 		</main>

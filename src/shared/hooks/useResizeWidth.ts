2a385011b8164a83357a9ef0805636d4d562977f
Michael <machapurin@icloud.com>
2025-06-05 11:31:00 +0300

    add adaptive for login, register pages

--- src/shared/hooks/useResizeWidth.ts
+++ src/shared/hooks/useResizeWidth.ts
@@ -1,20 +1,17 @@
 'use client'
 import { useState, useEffect } from 'react'
-// import { useDebounce } from './useDebounce'
+import { useDebounce } from './useDebounce'
 
 export function useResizeWidth(): number {
-  const [width, setWidth] = useState<number>(0)
+	const [width, setWidth] = useState<number>(0)
+	const handleResize = useDebounce(() => {
+		setWidth(window.innerWidth)
+	})
+	useEffect(() => {
+		handleResize()
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResize = () => {
-        setWidth(window.innerWidth)
-      }
-      handleResize()
-      window.addEventListener('resize', handleResize)
-      return () => window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
-  return width
+	return width
 }
